020195b5fb203366744e28239a22e856a9ca8988
Wills Manley <216224554+willsmanley@users.noreply.github.com>
2026-03-11 11:34:44 -0500

    misc

--- core/crates/ctx-http/tests/common/openai_responses_stub.rs
+++ core/crates/ctx-http/tests/common/openai_responses_stub.rs
@@ -9,7 +9,7 @@ use axum::http::{HeaderValue, StatusCode};
 use axum::response::Response;
 use axum::routing::{get, post};
 use axum::{Json, Router};
-use serde_json::{json, Value};
+use serde_json::{Value, json};
 use tokio::task::JoinHandle;
 
 #[derive(Clone)]
